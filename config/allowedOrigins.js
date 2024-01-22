d842d59f0e055b3c027957985a8ef97f8a216fdf
atis <atisozols5@gmail.com>
2024-01-22 21:45:19 +0200

    api url changed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    "https://skultes.cyclic.app",
-    'https://calm-tan-jackrabbit-slip.cyclic.app',
+    "https://skultesgym.onrender.com",
+    'https://skultesgym.lv',
     'http://127.0.0.1:5000',
     'http://localhost:5000'
 ];
