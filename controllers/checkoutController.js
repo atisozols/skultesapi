6c767f2b7da765bc72f99326f9f3f4cd4e113857
atis <atisozols5@gmail.com>
2024-01-25 00:22:02 +0200

    success url updated

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -23,8 +23,8 @@ const createCheckout = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             line_items: lineItems,
             mode: 'payment',
-            success_url: "https://skultes.cyclic.app/#/success?session_id={CHECKOUT_SESSION_ID}",
-            cancel_url: "https://skultes.cyclic.app",
+            success_url: "https://skultesgym.lv/#/success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url: "https://skultesgym.lv",
         });
 
         const appointments = req.body.map( appointment => {
