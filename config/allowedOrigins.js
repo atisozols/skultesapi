82556dfbccd83616fd0521bc9c1645bd6d007e4b
atis <atisozols5@gmail.com>
2024-01-25 20:07:02 +0200

    origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,8 +2,8 @@ const allowedOrigins = [
     "https://skultesgym.onrender.com",
     'https://skultesgym.lv',
     'https://skultes.cyclic.app',
-    'http://127.0.0.1:5000',
-    'http://localhost:5000'
+    'http://127.0.0.1',
+    'http://localhost'
 ];
 
 module.exports = allowedOrigins;
