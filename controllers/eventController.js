019b35d1c9a6234aa81cae7596b9aa95d57eb064
atis <atisozols5@gmail.com>
2024-03-20 11:29:29 +0200

    trying calendar fix

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,19 +1,19 @@
 const { google } = require('googleapis');
 // const key = require('../skultes-gym-service-account.json');
 
-// Set up JWT authentication
-const jwtClient = new google.auth.JWT(
-    process.env.GOOGLE_USER,
-    null,
-    process.env.GOOGLE_KEY,
-    ['https://www.googleapis.com/auth/calendar']
-);
-
-// Create a new calendar instance
-const calendar = google.calendar({ version: 'v3', auth: jwtClient });
-
 // Function to add an event to Google Calendar
 async function addEventToCalendar(eventDetails) {
+    // Set up JWT authentication
+    const jwtClient = new google.auth.JWT(
+        process.env.GOOGLE_USER,
+        null,
+        process.env.GOOGLE_KEY,
+        ['https://www.googleapis.com/auth/calendar']
+    );
+
+    // Create a new calendar instance
+    const calendar = google.calendar({ version: 'v3', auth: jwtClient });
+    
     // const eventDetails = {
     //     summary: 'Test Event',
     //     colorId: '3',
@@ -29,7 +29,7 @@ async function addEventToCalendar(eventDetails) {
     // };
 
     try {
-        const response = await calendar.events.insert({
+        const response = calendar.events.insert({
             calendarId: 'c4298fa15b1f42cc44bc7e3f834ab046f6ede03a346d67db5c2ae6f063293092@group.calendar.google.com', // Use the calendar ID of the fixed calendar you want to add events to
             resource: eventDetails
         });
